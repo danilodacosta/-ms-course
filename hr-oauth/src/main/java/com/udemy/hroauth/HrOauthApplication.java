528709846dda2e1e5d6b6a708c7525f6c7f53568
Danilo Pereira <danilo.pereira@desenvolvecidade.com>
2021-06-07 00:05:45 -0300

    UserFeignClient

--- hr-oauth/src/main/java/com/udemy/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/udemy/hroauth/HrOauthApplication.java
@@ -3,9 +3,12 @@ package com.udemy.hroauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+
 @EnableEurekaClient
+@EnableFeignClients
+@SpringBootApplication
 public class HrOauthApplication {
 
 	public static void main(String[] args) {
